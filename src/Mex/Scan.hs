b6328d536b3597a9940ca1e82dcf8bad686438c6
Mattia Barbon <mattia@barbon.org>
2016-04-17 20:24:50 +0200

    Fix relative paths when traversing directories

--- src/Mex/Scan.hs
+++ src/Mex/Scan.hs
@@ -3,24 +3,26 @@ module Mex.Scan (traverseWith) where
 import Control.Monad (liftM, foldM, forM_)
 import Data.List (partition)
 import System.Directory (getDirectoryContents, doesFileExist)
-import System.FilePath ((</>))
+import System.FilePath (makeRelative, (</>))
 
 traverseWith :: ([FilePath] -> IO a) -> FilePath -> IO ()
-traverseWith action base = do
-  entries <- liftM (filter interestingEntry) (getDirectoryContents base)
-  (files, directories) <- partitionPaths base (return entries)
-  action files
-  forM_ directories $ \directory -> traverseWith action (base </> directory)
+traverseWith action base = traverseWith_ action base base
+
+traverseWith_ action base current = do
+  entries <- liftM (filter interestingEntry) (getDirectoryContents current)
+  (files, directories) <- partitionPaths (return (map (current </>) entries))
+  action $ map (makeRelative base) files
+  forM_ directories $ \directory -> traverseWith_ action base (base </> directory)
   where
     interestingEntry "."  = False
     interestingEntry ".." = False
     interestingEntry _    = True
-    partitionPaths :: FilePath -> IO [FilePath] -> IO ([FilePath], [FilePath])
-    partitionPaths base paths = do
-      paths >>= (foldM (categorizePath base) ([], []))
-    categorizePath :: FilePath -> ([FilePath], [FilePath]) -> FilePath -> IO ([FilePath], [FilePath])
-    categorizePath base (files, directories) path = do
-      isFile <- doesFileExist (base </> path)
+    partitionPaths :: IO [FilePath] -> IO ([FilePath], [FilePath])
+    partitionPaths paths = do
+      paths >>= (foldM categorizePath ([], []))
+    categorizePath :: ([FilePath], [FilePath]) -> FilePath -> IO ([FilePath], [FilePath])
+    categorizePath (files, directories) path = do
+      isFile <- doesFileExist path
       return (if isFile
                 then (path:files, directories)
                 else (files, path:directories))
